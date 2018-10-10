b4ba5bc9726d2d35107a2badd84f7907504a99c1
Rui Sousa Portatil <rvcsousa@hotmail.com>
2018-10-10 21:55:18 +0000

    Power up

--- Hashtrobomb/src/org/academiadecodigo/bootcamp/Objects/Bomb/PowerUp.java
+++ Hashtrobomb/src/org/academiadecodigo/bootcamp/Objects/Bomb/PowerUp.java
@@ -4,9 +4,12 @@ import org.academiadecodigo.bootcamp.Field;
 import org.academiadecodigo.bootcamp.Objects.Destroyable;
 import org.academiadecodigo.bootcamp.Objects.GameObject;
 import org.academiadecodigo.bootcamp.Position.Position;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
 
 public class PowerUp extends GameObject implements Destroyable {
     private PowerUpTypes type;
+    private Picture firePower;
+    private String[] resource = {"resources/fireball.png"};
 
     public PowerUp(int x, int y, Field field){
         position = new Position(x, y, field);
